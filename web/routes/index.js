b9e51a888cd1342f7e86f432c433846c3bc3c3b5
4Moyede <krkdgud9@gmail.com>
2021-04-07 17:40:54 +0900

    [Ref] Change Promise code

--- web/routes/index.js
+++ web/routes/index.js
@@ -7,15 +7,16 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  googleAPI.download();
-  fetch("http://172.20.0.2:8000/api/data")
-    .then(data => {
-      data.json()
-        .then(contents => {
-          res.render('index.ejs', contents);
+  googleAPI.download()
+    .then(function () {
+      fetch("http://172.20.0.2:8000/api/data")
+        .then(data => {
+          data.json()
+            .then(contents => {
+              res.render('index.ejs', contents);
+            });
         });
     });
-
 });
 
 module.exports = router;
